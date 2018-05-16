71de3758eebd2c65e28c4da3a0e2a84f32d2a85f
Munia Akhter <muniakhter@gmail.com>
2018-05-17 00:01:19 +0600

    ems

---
+++ EMS/resources/views/footer.blade.php
@@ -0,0 +1,30 @@
+<div id="contact" class="container-fluid" style="background-color: orange; 
+color: white; margin-bottom: 0px; margin-top: 150px;">
+  <div class="row">
+    <div class="col-md-4">
+      <h3 class="text-left">Contract</h3>
+        <p class="text-left"><em>Total IT Solution</em></p>
+      <p><span class="glyphicon glyphicon-map-maker"></span> Badda, Dhaka</p>
+      <p> <span class="glyphicon glyphicon-phone"></span> Phone:+8801850969739</p>
+      <p><span class="glyphicon glyphicon-envelope"></span> Email:mahamudun013@gmail.com</p>
+   </div>
+
+    <div class="col-md-4" style="margin-top: 0px auto;">
+
+        <br>
+       <a href="#" style="font-size: 20px;"> <span class="glyphicon  glyphicon-envelope "> Facebook</span></a><br>
+       <a href="#" style="font-size: 20px;">  <span class="glyphicon  glyphicon-envelope "> Twitter</span></a><br>
+       <a href="#" style="font-size: 20px;"> <span class="glyphicon  glyphicon-envelope "> Linkedin</span></a><br>
+       <a href="#" style="font-size: 20px; "> <span class="glyphicon  glyphicon-envelope "> Skype</span></a><br>
+        
+
+
+       </div>
+    
+     <div class="col-md-4">
+       <br>
+       <div style="border: 1px solid red; height: 150px; width: 300px; margin: 0px auto;background-color: blue;text-align: center;">GOOGLE MAP</div>
+       <p style="font-size: 20px;text-align: center;">2018&copy<strong>Total IT Solutions</strong></p> 
+      </div>
+  </div>
+</div>
